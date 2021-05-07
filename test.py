cd5b2c3df14ac8cbefbcc93bc84334470cd6eb7d
ilong <liwenlong01@inke.cn>
2021-05-07 19:47:41 +0800

    [MOD]修改测试代码

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats
 
 if __name__ == '__main__':
    x = np.array([[1, 2, 3], [1, 4, 5], [1, 3, 3]])
@@ -14,11 +15,18 @@ if __name__ == '__main__':
    print("X", X)
    print(np.random.choice(range(10), 10, replace=False))
 
-   y_label = np.array([0, 2, 1, 2, 0, 0])
-   print("y_label",y_label)
-   y_onehot = np.zeros((y_label.shape[0], 3))
-   y_onehot[np.arange(y_label.shape[0]), y_label] = 1
 
-   print("y_onehot:", y_onehot)
-   print(np.argmax(y_onehot, axis=1))
+   p = np.asarray([0.65, 0.25, 0.07, 0.03])
+   q = np.array([0.6, 0.25, 0.1, 0.05])
+   q2 = np.array([0.1, 0.2, 0.3, 0.4])
+
+
+   def JS_divergence(p, q):
+      M = (p + q) / 2
+      return 0.5 * scipy.stats.entropy(p, M) + 0.5 * scipy.stats.entropy(q, M)
+
+
+   print(JS_divergence(p, q))  # 0.003093977084273652
+   print(JS_divergence(p, q2))  # 0.24719159952098618
+   print(JS_divergence(p, p))  # 0.0
 
